43c7c6d48d01e56dc665de59bf3ca00b709643d8
Flor <florxgomez@gmail.com>
2019-11-10 20:48:08 -0300

    initial commit

---
+++ meadowlark.js
@@ -0,0 +1,50 @@
+const express = require('express');
+var fortunes = [
+  'Conquer your fears or they will conquer you.',
+  'Rivers need springs.',
+  "Do not fear what you don't know.",
+  'You will have a pleasant surprise.',
+  'Whenever possible, keep it simple.'
+];
+
+const app = express();
+
+//set up handlebars view engine
+const handlebars = require('express-handlebars').create({
+  defaultLayout: 'main'
+});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/about', (req, res) => {
+  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render('about', { fortune: randomFortune });
+});
+
+app.use((req, res, ext) => {
+  res.status(404);
+  res.render('404');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(
+    'Express started on http://localhost:' +
+      app.get('port') +
+      '; press Ctrl+C to terminate.'
+  );
+});
